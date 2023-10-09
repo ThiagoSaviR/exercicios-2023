8d23ccd2aa6fb4cd9da60323ca6660fe3c450f3b
ThiagoSaviR <thiagosavir@gmail.com>
2023-10-09 00:12:55 -0300

    feat: cria header

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LanguageSelectorComponent } from './components/language-selector/language-selector.component';
+import { UserComponent } from './components/user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    SidebarComponent,
+    HeaderComponent,
+    LanguageSelectorComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule
